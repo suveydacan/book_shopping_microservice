6520a0c26728206ab04d0fb8ac4fb0702dff1f87
ZeynepIlkay <ilkayzeynepp@gmail.com>
2023-03-22 15:06:58 +0300

    odeme eklendi

---
+++ MyProject/pay/admin.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Payment(models.Model):
+    payment_method = models.CharField(max_length=30)
+   # payment_total =models.CharField(max_length=30)
+
+
+
+
+    def _str_(self):
+        return self.payment_method
