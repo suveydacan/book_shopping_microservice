70100d67cf1ab4fc02fd91fe7120656d45adefc8
Süveyda CAN <suveyda.2934@gmail.com>
2023-03-22 09:15:52 +0300

    User update edildi

--- MyProject/user/models.py
+++ MyProject/user/models.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+   # user=models.OneToOneField(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=30)
     surname=models.CharField(max_length=30)
     email=models.CharField(max_length=50)
@@ -21,13 +21,13 @@ class Profile(models.Model):
     addressDetail=models.TextField(max_length=30)
 
 
-    def __str__(self):
-        return str(self.user)
+    # def __str__(self):
+    #     return str(self.user)
 
-@receiver(post_save,sender=User)
-def create_or_upsate_user_profile(sender,instance,created,**kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+# @receiver(post_save,sender=User)
+# def create_or_upsate_user_profile(sender,instance,created,**kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     instance.profile.save()
 
     
