bc92c5165927cc6b64f400701ddc672e1c77823e
Süveyda CAN <suveyda.2934@gmail.com>
2023-03-24 17:36:43 +0300

    adres module eklendi

--- MyProject/user/admin.py
+++ MyProject/user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Profile
+from .models import MyAddress
 
 # Register your models here.
 
@@ -8,4 +9,8 @@ class ProfileAdmin(admin.ModelAdmin):
     # class Meta:
     #     model=Profile
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display=('user','city','county')
+
 admin.site.register(Profile)
+admin.site.register(MyAddress)
--- MyProject/user/models.py
+++ MyProject/user/models.py
@@ -8,26 +8,25 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 
 class Profile(models.Model):
-   # user=models.OneToOneField(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=30)
     surname=models.CharField(max_length=30)
     email=models.CharField(max_length=50)
     birthdate=models.DateField()
     phone=models.CharField(max_length=30)
     password=models.CharField(max_length=8)
-    city=models.CharField(max_length=30)
-    county=models.CharField(max_length=30)
-    postcode=models.IntegerField()
-    addressDetail=models.TextField(max_length=30)
+    # city=models.CharField(max_length=30)
+    # county=models.CharField(max_length=30)
+    # postcode=models.IntegerField()
+    # addressDetail=models.TextField(max_length=30)
 
 
-    # def __str__(self):
-    #     return str(self.user)
-
-# @receiver(post_save,sender=User)
-# def create_or_upsate_user_profile(sender,instance,created,**kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     instance.profile.save()
+class MyAddress(models.Model):
+     user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+     city=models.CharField(max_length=30)
+     county=models.CharField(max_length=30)
+     neighborhood=models.CharField(max_length=30)
+     street=models.CharField(max_length=30)
+     postcode=models.IntegerField()
+     addressDetail=models.TextField(max_length=60)
 
     
