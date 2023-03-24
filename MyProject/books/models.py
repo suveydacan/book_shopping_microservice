16659e018a7919b8430a051fbe78faa1d9c1bc5e
Süveyda CAN <suveyda.2934@gmail.com>
2023-03-24 17:18:08 +0300

    stok eklendi

--- MyProject/books/models.py
+++ MyProject/books/models.py
@@ -9,6 +9,7 @@ class Book(models.Model):
     category=models.CharField(max_length=50)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     page_number=models.IntegerField()
+    stocks=models.IntegerField(default=0)
     
 
    
