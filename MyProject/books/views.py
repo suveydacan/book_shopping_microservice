3084a1f5a49c3b89bf29e3215e8eeee6e0ae7446
Süveyda CAN <suveyda.2934@gmail.com>
2023-04-15 00:12:36 +0300

    book detail yapıldı

--- MyProject/books/views.py
+++ MyProject/books/views.py
@@ -10,3 +10,14 @@ def product_pages(request):
     }
 
     return render(request ,"books/product.html", context)
+
+
+def book_detail(request, book_id):
+    selectedBook=Book.objects.get(id=book_id)
+
+    # for book in books:
+    #     if book.pk==id:
+    #         selectedBook=book
+    return render(request, "books/detail.html",{
+        "book":selectedBook
+    })
--- MyProject/orders/urls.py
+++ MyProject/orders/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('checkout', views.checkout, name='checkout'),
-    path('detail', views.detail, name='detail'),
-    path('order_detail', views.order_detail, name='order_detail'),
+    path('order_details', views.order_details, name='order_details'),
     path('orders', views.orders, name='orders'),
     path('shopping-cart', views.shopping_cart, name='shopping-cart'),
    
